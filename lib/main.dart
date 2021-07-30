274aa0f2054ede7f80dbca7bec98296eb09d7902
Erin.K <yjkang.dev@gmail.com>
2021-07-30 16:05:44 +0900

    <add>: 더보기 화면 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inflearn_netflix_clone/screen/home_screen.dart';
+import 'package:inflearn_netflix_clone/screen/more_screen.dart';
 import 'package:inflearn_netflix_clone/widget/bottom_bar.dart';
 
 void main() => runApp(MyApp());
@@ -27,7 +28,7 @@ class _MyAppState extends State<MyApp> {
                 HomeScreen(),
                 Container(child: Center(child:Text('search'))),
                 Container(child: Center(child:Text('save'))),
-                Container(child: Center(child:Text('more'))),
+                MoreScreen(),
               ],
             ),
             bottomNavigationBar: Bottom()
